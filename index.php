ab79e66ceb3f1d29e11ad6b197fbbc56f004aba0
Pavel-Lukyanov <97879761+Pavel-Lukyanov@users.noreply.github.com>
2022-12-21 17:44:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,53 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+}
+if (isset($_SESSION['count']) && $_SESSION['count'] >= 1) {
+    echo 'Ошибка!<br>Кол-во файлов больше 1';
+} else { ?>
+    <!DOCTYPE html>
+    <html lang="ru">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <form action="./send_photo.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="photo">
+            <button type="submit">
+                Отправить
+            </button>
+        </form>
+    </body>
+
+    </html>
+<?php }
+
 if (isset($_FILES['photo'])) {
     try {
-        move_uploaded_file($_FILES['photo']['tmp_name'], './img/' . $_FILES['photo']['name']);
-?>  
 
-    <img src="<?= './img/'.$_FILES['photo']['name']; ?>">
+        $path_info = pathinfo($_FILES['photo']['name']);
 
-<?php
+
+        if (($path_info['extension'] == 'jpg') || $path_info['extension']  == 'png') {
+            if ($_FILES['photo']["size"] <= 2097152) {
+                move_uploaded_file($_FILES['photo']['tmp_name'], './img/' . $_FILES['photo']['name']);
+                $_SESSION['count']++;
+                header('Location: ./img/' . $_FILES['photo']['name'] . '');
+            } else {
+                echo 'Максимальный размер файла должен быть не более 2 мб!';
+            }
+        } else {
+            echo 'Поддерживаемые расширения файла: jpg, png';
+        }
     } catch (Exception $e) {
         echo $e->getMessage();
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="./index.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="photo">
-        <button type="submit">
-            Отправить
-        </button>
-    </form>
-</body>
-
-</html>
